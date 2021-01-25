8c48be883fc4ad67eb61e9f99e2a0bdf11d979a4
Michael Gilbert <gilbertm@mit.edu>
2021-01-25 16:21:19 -0500

    Add other offsets for future use and add stack bp, sp offset

--- bhive/common.h
+++ bhive/common.h
@@ -4,15 +4,13 @@
 /* Address of page to be used for child stack. */
 #define AUX_MEM_ADDR (void *)0x0000700000000000
 #define AUX_MEM_ADDR_STR "0x0000700000000000"
-#define STACK_PAGE_ADDR (void *)0x0000700000001000
-#define STACK_PAGE_ADDR_STR "0x0000700000001000"
 
 #define ITERATIONS 50
 
 #define INIT_VALUE 0x2324000
 #define INIT_VALUE_STR "0x2324000"
 
-#define MAX_FAULTS 100
+#define MAX_FAULTS 5
 
 #define PAGE_SHIFT 12
 #define PAGE_SIZE (1u << PAGE_SHIFT)
@@ -22,11 +20,20 @@
 /* Size of shared memory. First page will be mapped to child virtual address
  * pages. Second page mapped to child stack page.
  */
-#define SHARED_MEM_SIZE 3 * PAGE_SIZE
+#define SHARED_MEM_SIZE 2 * PAGE_SIZE
 
 #define CYC_COUNT_OFFSET 0
-#define PERF_FD_OFFSET 8
-#define TEST_PAGE_END_OFFSET 16
-#define ITERATIONS_OFFSET 24
+#define L1_READ_MISSES_OFFSET 8
+#define L1_WRITE_MISSES_OFFSET 16
+#define ICACHE_MISSES_OFFSET 24
+#define CTX_SWITCHES_OFFSET 32
+
+#define PERF_FD_OFFSET 40
+#define TEST_PAGE_END_OFFSET 48
+#define ITERATIONS_OFFSET 56
+#define STACK_BP_OFFSET 64
+#define STACK_BP_OFFSET_STR "64"
+#define STACK_SP_OFFSET 72
+#define STACK_SP_OFFSET_STR "72"
 
 #endif // _COMMON_H_
