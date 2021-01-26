cfc41d6e4c9b5fab6abbb5bf58b631dfdf932bfa
Michael Gilbert <gilbertm@mit.edu>
2021-01-26 11:30:35 -0500

    No need for STR versions of macros anymore. Store MAP_AND_RESTART addr in aux. mem

--- bhive/common.h
+++ bhive/common.h
@@ -3,18 +3,15 @@
 
 /* Address of page to be used for child stack. */
 #define AUX_MEM_ADDR (void *)0x0000700000000000
-#define AUX_MEM_ADDR_STR "0x0000700000000000"
 
 #define ITERATIONS 50
 
 #define INIT_VALUE 0x2324000
-#define INIT_VALUE_STR "0x2324000"
 
 #define MAX_FAULTS 5
 
 #define PAGE_SHIFT 12
 #define PAGE_SIZE (1u << PAGE_SHIFT)
-#define HALF_PAGE_STR "2048"
 
 #define SHM_FD 42
 /* Size of shared memory. First page will be mapped to child virtual address
@@ -32,8 +29,7 @@
 #define TEST_PAGE_END_OFFSET 48
 #define ITERATIONS_OFFSET 56
 #define STACK_BP_OFFSET 64
-#define STACK_BP_OFFSET_STR "64"
 #define STACK_SP_OFFSET 72
-#define STACK_SP_OFFSET_STR "72"
+#define MAP_AND_RESTART_ADDR_OFFSET 80
 
 #endif // _COMMON_H_
