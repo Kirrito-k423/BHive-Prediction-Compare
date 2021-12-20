e3bdca854bd6513e0abc5d4730ea1756f28afb8f
shaojiemike <shaojiemike@mail.ustc.edu.cn>
2021-12-20 21:11:01 +0800

    add CP and wrong out

--- pythonTest/OSACAVSBhive_AnalyseSpeedUp_sudo.py
+++ pythonTest/OSACAVSBhive_AnalyseSpeedUp_sudo.py
@@ -14,19 +14,25 @@ from multiprocessing import Process, Queue
 BHiveCount=10000
 ProcessNum=40
 
-def OSACA(inputFile):
+def OSACA(inputFile,maxOrCP):
     val=os.popen('/home/shaojiemike/test/OSACA/osacaEnv/bin/osaca --arch TSV110 '+str(inputFile))#  -timeline -show-encoding -all-stats -all-views
     list = val.readlines()
     for listObj in list[20:]:
         # print(listObj)
-        regexResult=re.search("max_tp_sum:([.0-9]*),min_tp_sum:([.0-9]*),avg_tp_sum:([.0-9]*)",listObj)# 需要修改osaca来有个标记位匹配
+        regexResult=re.search("max_tp_sum:([.0-9]*),min_tp_sum:([.0-9]*),avg_tp_sum:([.0-9]*),cp_sum:([.0-9]*)",listObj)# 需要修改osaca来有个标记位匹配
         if regexResult:
             maxresultCycle=regexResult.group(1)
             minresultCycle=regexResult.group(2)
             avgresultCycle=regexResult.group(3)
+            CPresultCycle=regexResult.group(4)
             # print("before mca {}/{} cycle:{}".format(order,num_file,resultCycle))
             # print("  maxresultCycle :{}".format(maxresultCycle))
-            return maxresultCycle
+            if maxOrCP == "max":
+                return maxresultCycle
+            elif maxOrCP == "CP":
+                return CPresultCycle
+            else:
+                return -1
     # print("  wrong")
     return -1
 
@@ -122,18 +128,20 @@ def calculateAccuracy(accurateCycles,predictionCycles,rank):
     else:
         # print(" rank{}-tsj2".format(rank))
         # print("{} {}".format(accurateCycles,predictionCycles))
-        gap=abs(float(predictionCycles)-int(accurateCycles))
+        gap=abs(BHiveCount*float(predictionCycles)-int(accurateCycles))
         # print(" rank{}-{}".format(rank,int(gap)/int(accurateCycles)))
         return int(gap)/int(accurateCycles) # accuracy variable is error
 
-def paralleReadProcess(rank,password, startFileLine,endFileLine,unique_revBiblock_Queue,frequencyRevBiBlock_Queue,OSACACyclesRevBiBlock_Queue,BhiveCyclesRevBiBlock_Queue,accuracy_Queue):
+def paralleReadProcess(rank,password, startFileLine,endFileLine,unique_revBiblock_Queue,frequencyRevBiBlock_Queue,OSACAmaxCyclesRevBiBlock_Queue,OSACACPCyclesRevBiBlock_Queue,BhiveCyclesRevBiBlock_Queue,accuracyMax_Queue,accuracyCP_Queue):
     print("MPI Process Start {:2d} {}~{}".format(rank,startFileLine,endFileLine))
     fread=open(filename, 'r')
     unique_revBiblock=set()
     frequencyRevBiBlock = defaultdict(int)
-    OSACACyclesRevBiBlock = defaultdict(int)
+    OSACAmaxCyclesRevBiBlock = defaultdict(int)
+    OSACACPCyclesRevBiBlock = defaultdict(int)
     BhiveCyclesRevBiBlock = defaultdict(int)
-    accuracy = defaultdict(float)
+    accuracyMax = defaultdict(float)
+    accuracyCP = defaultdict(float)
     for line in tqdm(fread.readlines()[startFileLine:endFileLine],total=endFileLine-startFileLine,desc=str("{:2d}".format(rank))):
     # for line in fread:
         # print("rank{}".format(rank))
@@ -144,22 +152,27 @@ def paralleReadProcess(rank,password, startFileLine,endFileLine,unique_revBibloc
         # print("     rank{}:block{}".format(rank,block))
         BhiveCyclesRevBiBlock[block] = BHive(password,BHiveInput(block),BHiveInputDel0x(block),0)
         # print("         rank{}:block{}______{}".format(rank,block,BhiveCyclesRevBiBlock[block]))
-        OSACACyclesRevBiBlock[block] = OSACA(saveOSACAInput2File(capstoneList(capstoneInput(block)),rank))
-        # print("             rank{}:block{}______{}".format(rank,block,OSACACyclesRevBiBlock[block]))
-        accuracy[block]= calculateAccuracy(BhiveCyclesRevBiBlock[block],OSACACyclesRevBiBlock[block],rank)
+        OSACAInput=saveOSACAInput2File(capstoneList(capstoneInput(block)),rank)
+        OSACAmaxCyclesRevBiBlock[block] = OSACA(OSACAInput,"max")
+        OSACACPCyclesRevBiBlock[block] = OSACA(OSACAInput,"CP")
+        # print("             rank{}:block{}______{}".format(rank,block,OSACAmaxCyclesRevBiBlock[block]))
+        accuracyMax[block]= calculateAccuracy(BhiveCyclesRevBiBlock[block],OSACAmaxCyclesRevBiBlock[block],rank)
+        accuracyCP[block]= calculateAccuracy(BhiveCyclesRevBiBlock[block],OSACACPCyclesRevBiBlock[block],rank)
         # print("0rank{}".format(rank))
     fread.close() 
     # print("1rank{}".format(rank))
     unique_revBiblock_Queue.put(unique_revBiblock)
     frequencyRevBiBlock_Queue.put(frequencyRevBiBlock)
     # print("2rank{}".format(rank))
-    OSACACyclesRevBiBlock_Queue.put(OSACACyclesRevBiBlock)
+    OSACAmaxCyclesRevBiBlock_Queue.put(OSACAmaxCyclesRevBiBlock)
+    OSACACPCyclesRevBiBlock_Queue.put(OSACACPCyclesRevBiBlock)
     BhiveCyclesRevBiBlock_Queue.put(BhiveCyclesRevBiBlock)
     # print("3rank{}".format(rank))
-    accuracy_Queue.put(accuracy)
+    accuracyMax_Queue.put(accuracyMax)
+    accuracyCP_Queue.put(accuracyCP)
     print("MPI Process end {:2d} {}~{}".format(rank,startFileLine,endFileLine))
 
-def readPartFile(password, unique_revBiblock,frequencyRevBiBlock,OSACACyclesRevBiBlock,BhiveCyclesRevBiBlock,accuracy):
+def readPartFile(password, unique_revBiblock,frequencyRevBiBlock,OSACAmaxCyclesRevBiBlock,OSACACPCyclesRevBiBlock,BhiveCyclesRevBiBlock,accuracyMax,accuracyCP):
     # print(filename)
     # with open(filename, 'r') as f:#with语句自动调用close()方法
     #     line = f.readline()
@@ -178,15 +191,17 @@ def readPartFile(password, unique_revBiblock,frequencyRevBiBlock,OSACACyclesRevB
     fread.close() 
     unique_revBiblock_Queue =Queue()
     frequencyRevBiBlock_Queue = Queue()
-    OSACACyclesRevBiBlock_Queue=Queue()
+    OSACAmaxCyclesRevBiBlock_Queue=Queue()
+    OSACACPCyclesRevBiBlock_Queue=Queue()
     BhiveCyclesRevBiBlock_Queue=Queue()
-    accuracy_Queue=Queue()
+    accuracyMax_Queue=Queue()
+    accuracyCP_Queue=Queue()
 
 
     for i in range(ProcessNum):
         startFileLine=int(i*num_file/ProcessNum)
         endFileLine=int((i+1)*num_file/ProcessNum)
-        p = Process(target=paralleReadProcess, args=(i,password, startFileLine,endFileLine,unique_revBiblock_Queue,frequencyRevBiBlock_Queue,OSACACyclesRevBiBlock_Queue,BhiveCyclesRevBiBlock_Queue,accuracy_Queue))
+        p = Process(target=paralleReadProcess, args=(i,password, startFileLine,endFileLine,unique_revBiblock_Queue,frequencyRevBiBlock_Queue,OSACAmaxCyclesRevBiBlock_Queue,OSACACPCyclesRevBiBlock_Queue,BhiveCyclesRevBiBlock_Queue,accuracyMax_Queue,accuracyCP_Queue))
         p.start()
 
     while unique_revBiblock_Queue.qsize()<ProcessNum:
@@ -198,38 +213,63 @@ def readPartFile(password, unique_revBiblock,frequencyRevBiBlock,OSACACyclesRevB
         print("MPISum rank : {}, blockNum : {},leftQueueNum : {}".format(i,len(unique_revBiblock),unique_revBiblock_Queue.qsize()))
         unique_revBiblock=unique_revBiblock.union(unique_revBiblock_Queue.get())
         frequencyRevBiBlock.update(frequencyRevBiBlock_Queue.get())
-        OSACACyclesRevBiBlock.update(OSACACyclesRevBiBlock_Queue.get())
+        OSACAmaxCyclesRevBiBlock.update(OSACAmaxCyclesRevBiBlock_Queue.get())
+        OSACACPCyclesRevBiBlock.update(OSACACPCyclesRevBiBlock_Queue.get())
         BhiveCyclesRevBiBlock.update(BhiveCyclesRevBiBlock_Queue.get())
-        accuracy.update(accuracy_Queue.get())
+        accuracyMax.update(accuracyMax_Queue.get())
+        accuracyCP.update(accuracyCP_Queue.get())
     return unique_revBiblock
     # print(unique_revBiblock)
     # print(frequencyRevBiBlock)
-    # print(accuracy)
+    # print(accuracyMax)
     # print(len(unique_revBiblock))
     # print(len(frequencyRevBiBlock))
     
 
 
-def saveAllResult(taskfilenameprefix,unique_revBiblock,frequencyRevBiBlock,OSACACyclesRevBiBlock,BhiveCyclesRevBiBlock,accuracy):
+def saveAllResult(taskfilenameprefix,unique_revBiblock,frequencyRevBiBlock,OSACAmaxCyclesRevBiBlock,OSACACPCyclesRevBiBlock,BhiveCyclesRevBiBlock,accuracyMax,accuracyCP):
     writeFilename="{}_count{}.OSACAVSBHive".format(taskfilenameprefix,BHiveCount)
+    wrongResultFilename="{}_count{}.OSACAVSBHive_wrongResult".format(taskfilenameprefix,BHiveCount)
     fwriteblockfreq = open(writeFilename, "w")
+    wrongResultFile = open(wrongResultFilename, "w")
     validNum=0
     unvalidNum=0
-    totalAccuracy=0.0
+    totalaccuracyMax=0.0
+    totalAccuracyCP=0.0
+    lineNum=0
     for tmp_block_binary_reverse in unique_revBiblock:
-        fwriteblockfreq.writelines(tmp_block_binary_reverse+','+str(frequencyRevBiBlock[tmp_block_binary_reverse])+','+ \
-            str(OSACACyclesRevBiBlock[tmp_block_binary_reverse])+' _ '+
-            str(BhiveCyclesRevBiBlock[tmp_block_binary_reverse])+' _ '+
-            str(accuracy[tmp_block_binary_reverse])+"\n")
-        if accuracy[tmp_block_binary_reverse] != 0:
+        lineNum+=1
+        if accuracyMax[tmp_block_binary_reverse] != 0 and accuracyCP[tmp_block_binary_reverse] != 0:
             validNum+=frequencyRevBiBlock[tmp_block_binary_reverse]
-            totalAccuracy+=frequencyRevBiBlock[tmp_block_binary_reverse]*accuracy[tmp_block_binary_reverse]
+            totalaccuracyMax+=frequencyRevBiBlock[tmp_block_binary_reverse]*accuracyMax[tmp_block_binary_reverse]
+            totalaccuracyCP+=frequencyRevBiBlock[tmp_block_binary_reverse]*accuracyCP[tmp_block_binary_reverse]
+            fwriteblockfreq.writelines("{:5d} ".format(lineNum)+tmp_block_binary_reverse+','+str(frequencyRevBiBlock[tmp_block_binary_reverse])+',max '+ \
+                str(OSACAmaxCyclesRevBiBlock[tmp_block_binary_reverse])+' _CP '+
+                str(OSACACPCyclesRevBiBlock[tmp_block_binary_reverse])+' _Bh '+
+                str(BhiveCyclesRevBiBlock[tmp_block_binary_reverse])+' _ '+
+                str(accuracyMax[tmp_block_binary_reverse])+' _ '+
+                str(accuracyCP[tmp_block_binary_reverse])+"\n")
         else:
             unvalidNum+=1
+            fwriteblockfreq.writelines("{:5d} ".format(lineNum)+tmp_block_binary_reverse+','+str(frequencyRevBiBlock[tmp_block_binary_reverse])+',max '+ \
+                str(OSACAmaxCyclesRevBiBlock[tmp_block_binary_reverse])+' _CP '+
+                str(OSACACPCyclesRevBiBlock[tmp_block_binary_reverse])+' _Bh '+
+                str(BhiveCyclesRevBiBlock[tmp_block_binary_reverse])+' _ '+
+                str(accuracyMax[tmp_block_binary_reverse])+' _ '+
+                str(accuracyCP[tmp_block_binary_reverse])+" wrong\n")
+            wrongResultFile.writelines("{:5d} ".format(lineNum)+tmp_block_binary_reverse+','+str(frequencyRevBiBlock[tmp_block_binary_reverse])+',max '+ \
+                str(OSACAmaxCyclesRevBiBlock[tmp_block_binary_reverse])+' _CP '+
+                str(OSACACPCyclesRevBiBlock[tmp_block_binary_reverse])+' _Bh '+
+                str(BhiveCyclesRevBiBlock[tmp_block_binary_reverse])+' _ '+
+                str(accuracyMax[tmp_block_binary_reverse])+' _ '+
+                str(accuracyCP[tmp_block_binary_reverse])+" wrong\n")
     fwriteblockfreq.writelines("validTotalNum & unvalidBlockNum :"+str(validNum)+" "+str(unvalidNum)+"\n")
-    fwriteblockfreq.writelines("avg accuracy is "+str(totalAccuracy/validNum))
-    print("avg error rate is "+str(totalAccuracy/validNum)) 
+    fwriteblockfreq.writelines("avg accuracyMax is "+str(totalaccuracyMax/validNum))
+    fwriteblockfreq.writelines("avg accuracyCP is "+str(totalaccuracyCP/validNum))
+    print("avg Max error rate is "+str(totalaccuracyMax/validNum)) 
+    print("avg CP error rate is "+str(totalaccuracyCP/validNum)) 
     fwriteblockfreq.close()
+    wrongResultFile.close()
 
 if __name__ == "__main__":
     global filename
@@ -242,10 +282,12 @@ if __name__ == "__main__":
     filename="{}.{}".format(taskfilenameprefix,taskfilenamesubfix)
     unique_revBiblock=set()
     frequencyRevBiBlock = defaultdict(int)
-    # OSACACyclesRevBiBlock = defaultdict(int)
-    OSACACyclesRevBiBlock = defaultdict(int)
+    # OSACAmaxCyclesRevBiBlock = defaultdict(int)
+    OSACAmaxCyclesRevBiBlock = defaultdict(int)
+    OSACACPCyclesRevBiBlock = defaultdict(int)
     BhiveCyclesRevBiBlock = defaultdict(int)
-    accuracy = defaultdict(float)
-    unique_revBiblock=readPartFile(password, unique_revBiblock,frequencyRevBiBlock,OSACACyclesRevBiBlock,BhiveCyclesRevBiBlock,accuracy)
+    accuracyMax = defaultdict(float)
+    accuracyCP = defaultdict(float)
+    unique_revBiblock=readPartFile(password, unique_revBiblock,frequencyRevBiBlock,OSACAmaxCyclesRevBiBlock,OSACACPCyclesRevBiBlock,BhiveCyclesRevBiBlock,accuracyMax,accuracyCP)
     print("blockSize {} {}".format(len(unique_revBiblock),len(frequencyRevBiBlock)))
-    saveAllResult(taskfilenameprefix,unique_revBiblock,frequencyRevBiBlock,OSACACyclesRevBiBlock,BhiveCyclesRevBiBlock,accuracy)
+    saveAllResult(taskfilenameprefix,unique_revBiblock,frequencyRevBiBlock,OSACAmaxCyclesRevBiBlock,OSACACPCyclesRevBiBlock,BhiveCyclesRevBiBlock,accuracyMax,accuracyCP)
